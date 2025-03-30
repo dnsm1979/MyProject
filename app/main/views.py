12f8f398e277553bc8f407ae9fea7cc363f2fabe
dnsm1979 <dnsm@rambler.ru>
2025-03-30 20:33:16 +0500

    123

---
+++ app/main/views.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+
+
+class IndexView(TemplateView):
+    template_name = 'main/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
+class AboutView(TemplateView):
+    template_name = 'main/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
